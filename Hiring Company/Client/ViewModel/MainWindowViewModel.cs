b186d5d1823aa75aebbfce9aef9e64ca2900f9aa
milosgagovic <gagovicmilosgagovic@gmail.com>
2017-04-20 21:41:52 +0200

    Also addes comment on Hiring

--- Hiring Company/Client/ViewModel/MainWindowViewModel.cs
+++ Hiring Company/Client/ViewModel/MainWindowViewModel.cs
@@ -31,8 +31,8 @@ namespace Client.ViewModel
             string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
             string path = System.IO.Path.GetDirectoryName(executable);
             path = path.Substring(0, path.LastIndexOf("NMMS")) + "NMMS/Common";
-            EditIcon = new BitmapImage(new Uri(path + "/Images/edit.png"));
-            RemoveIcon = new BitmapImage(new Uri(path + "/Images/delete.png"));
+           // EditIcon = new BitmapImage(new Uri(path + "/Images/edit.png"));
+            //RemoveIcon = new BitmapImage(new Uri(path + "/Images/delete.png"));
             Proxy = App.Proxy;
 
         }
